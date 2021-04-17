601a5c2db71a6a2c13c9ce93277ca5895fa68aea
Andras Serfozo <subztep@gmail.com>
2021-04-17 09:40:35 +0100

    test

--- chalkin-test.js
+++ chalkin-test.js
@@ -1,4 +1,6 @@
-// import chalkin from "https://deno.land/x/chalkin@v0.1.3/mod.ts"
-import chalkin from "./chalkin_mod.ts"
+import chalkin from "https://deno.land/x/chalkin@v0.1.3/mod.ts"
+//import chalkin from "./chalkin_mod.ts"
 
 console.log(chalkin.green("Hello, World!"))
+// console.log(chalkin["green"])
+// console.log(chalkin.werty)
