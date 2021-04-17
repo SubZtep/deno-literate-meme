1c083da67c24101897bd72233f831b1f472ffba0
Andras Serfozo <subztep@gmail.com>
2021-04-17 11:38:29 +0100

    test

--- chalkin-test.js
+++ chalkin-test.js
@@ -1,5 +1,8 @@
 import { chalkin as c } from "https://deno.land/x/chalkin@v0.1.3/mod.ts";
 
+console.log("CCCCC", c)
+Deno.exit(0)
+
 console.log("XXX", Deno.env.toObject())
 
 const handler = {
@@ -8,7 +11,8 @@ const handler = {
   }
 }
 
-const chalkin = false ? c : new Proxy({}, handler)
+// const chalkin = false ? c : new Proxy({}, handler)
+const chalkin = new Proxy({}, handler)
 
 console.log(chalkin.green("Hello, World!"));
 console.log(chalkin.green.bgRed("Hello, World!"));
